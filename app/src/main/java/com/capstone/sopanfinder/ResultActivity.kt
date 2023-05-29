4eda7b37741af83e59e780f666a0d8570a770309
Christine <christine1@student.umn.ac.id>
2023-05-29 15:33:46 +0700

    home page and result page xml

---
+++ app/src/main/java/com/capstone/sopanfinder/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.sopanfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
