133a312f139066f8df66996948be5561966541a9
Christine <christine1@student.umn.ac.id>
2023-06-01 20:12:52 +0700

    change variable to val

--- app/src/main/java/com/capstone/sopanfinder/view/maps/MapsActivity.kt
+++ app/src/main/java/com/capstone/sopanfinder/view/maps/MapsActivity.kt
@@ -33,20 +33,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        var lat = intent.getDoubleExtra("Latitude", 0.0)
-        var lon = intent.getDoubleExtra("Longitude", 0.0)
+        val lat = intent.getDoubleExtra("Latitude", 0.0)
+        val lon = intent.getDoubleExtra("Longitude", 0.0)
 
         // Add a marker in Sydney and move the camera
         val location = LatLng(lat, lon)
