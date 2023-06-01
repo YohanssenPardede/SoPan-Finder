b98c08fc3a73cb0c6cb7a40f0152919e5e4774b7
Christine <christine1@student.umn.ac.id>
2023-06-01 17:07:01 +0700

    fetch user latitude and longitude, maps confirmation activity

--- app/src/main/java/com/capstone/sopanfinder/view/home/HomeActivity.kt
+++ app/src/main/java/com/capstone/sopanfinder/view/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.capstone.sopanfinder.view.home
 
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,6 +12,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.capstone.sopanfinder.view.profile.ProfileActivity
 import com.capstone.sopanfinder.R
 import com.capstone.sopanfinder.view.result.ResultActivity
@@ -18,33 +20,73 @@ import com.capstone.sopanfinder.databinding.ActivityHomeBinding
 import com.capstone.sopanfinder.preference.UserPreference
 import com.capstone.sopanfinder.view.favorite.FavoriteActivity
 import com.capstone.sopanfinder.view.login.LoginActivity
+import com.capstone.sopanfinder.view.maps.MapsActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import java.util.*
 import kotlin.concurrent.schedule
 
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+
+    var lat : Double = 0.0
+    var lon : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         loginCheck()
 
         binding.searchBtn.setOnClickListener{
+            getLocation()
+
             val animation: Animation =
                 AnimationUtils.loadAnimation(applicationContext, R.anim.blink)
                 binding.searchBtn.startAnimation(animation)
                 binding.homeStatus.setText(R.string.findsopan2)
                 binding.homeDesc.setText(R.string.wait)
 
+
             Timer().schedule(2000) {
                 binding.searchBtn.clearAnimation()
-                startActivity(Intent(this@HomeActivity, ResultActivity::class.java))
+
+                val intent = Intent(this@HomeActivity, MapsActivity::class.java)
+                intent.putExtra("Latitude", lat)
+                intent.putExtra("Longitude", lon)
+                startActivity(intent)
+
+//                startActivity(Intent(this@HomeActivity, ResultActivity::class.java))
             }
         }
     }
+
+    private fun getLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it!= null){
+                Toast.makeText(applicationContext, "${it.latitude} ${it.longitude}", Toast.LENGTH_SHORT).show()
+                lat = it.latitude
+                lon = it.longitude
+
+            }
+        }
+    }
+
 
     private fun loginCheck() {
         if (UserPreference.getInstance(this).isLoggedIn) {
---
+++ app/src/main/java/com/capstone/sopanfinder/view/maps/MapsActivity.kt
@@ -0,0 +1,59 @@
+package com.capstone.sopanfinder.view.maps
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.sopanfinder.R
+import com.capstone.sopanfinder.databinding.ActivityMapsBinding
+import com.capstone.sopanfinder.view.result.ResultActivity
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        var lat = intent.getDoubleExtra("Latitude", 0.0)
+        var lon = intent.getDoubleExtra("Longitude", 0.0)
+
+        // Add a marker in Sydney and move the camera
+        val location = LatLng(lat, lon)
+        mMap.addMarker(MarkerOptions().position(location).title("You Are Here!"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+
+        binding.confirmButton.setOnClickListener{
+            startActivity(Intent(this@MapsActivity, ResultActivity::class.java))
+        }
+
+    }
+}
