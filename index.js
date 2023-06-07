f0ea271c77430130caf1d46f520851ff313234df
rizkyjoel <112708386+rizkyjoel@users.noreply.github.com>
2023-06-07 18:12:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 dotenv.config();
 const app = express();
-
 try {
     await db.authenticate();
     console.log('Database Connceted..')
@@ -14,9 +14,11 @@ try {
     console.log(error);
 }
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5000'}));
-app.use(cookieParser());
+app.use(cors({ credentials: true, origin: '0.0.0.0'}))
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
+app.use(cookieParser());
 
-app.listen(5000, () => console.log('Server running at port 5000'));
+app.listen(8080, () => 
+    console.log('Server running at port 8080'));
