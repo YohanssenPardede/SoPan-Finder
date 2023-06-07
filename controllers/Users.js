c682df67f5d540834731314739b0a1241df860d4
rizkyjoel <112708386+rizkyjoel@users.noreply.github.com>
2023-06-07 18:10:49 +0800

    Update Users.js
    menampilkan data user dalam format .json pada saat login

--- controllers/Users.js
+++ controllers/Users.js
@@ -2,18 +2,18 @@ import Users from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const getUsers = async(req, res) => {
+export const getUsers = (async(req, res)=> {
     try {
         const users = await Users.findAll({
-            attributes:['id', 'name', 'email']
+            attributes: ['id', 'name', 'email']
         });
         res.json(users);
     } catch (error) {
         console.log(error);
     }
-}
+});
 
-export const Register = async(req, res) => {
+export const Register = (async(req, res) => {
     
         const { name, email, password, confirmPassword } = req.body;
         const salt = await bcrypt.genSalt();
@@ -40,40 +40,60 @@ export const Register = async(req, res) => {
         res.json({ msg: "Registered successfully" });
     } catch (error) {
         console.log(error);
-    }};
+    }
+});
 
-    export const Login = (async(req, res) => {
-        try {
-            const user = await Users.findAll({
-                where: {
-                    email: req.body.email
-                }
-            });
-            const match = await bcrypt.compare(req.body.password, user[0].password);
-            if(!match) return res.status(400).json({msg: "Wrong password"});
-            const userId = user[0].id;
-            const name = user[0].name;
-            const email = user[0].email;
-            const accessToken = jwt.sign({userId, name, email}, process.env.ACCESS_TOKEN_SECRET,{
-                expiresIn: '30s'
-            });
-            const refreshToken = jwt.sign({userId, name, email}, process.env.REFRESH_TOKEN_SECRET, {
-                expiresIn: '1d'
-            });
-            await Users.update({refresh_token:refreshToken}, {
-                where: {
-                    id: userId
-                }
-            });
-            res.cookie('refreshToken', refreshToken, {
-                httpOnly: true,
-                maxAge: 24 * 60 * 60 * 1000,
-            });
-            res.json({ accessToken });
-        } catch (error) {
-            res.status(404).json({msg: "Email not found"});
+export const Login = async (req, res) => {
+    try {
+      const user = await Users.findAll({
+        where: {
+          email: req.body.email
         }
-    });
+      });
+  
+      if (user.length === 0) {
+        return res.status(404).json({ msg: "Email not found" });
+      }
+  
+      const match = await bcrypt.compare(req.body.password, user[0].password);
+      if (!match) {
+        return res.status(400).json({ msg: "Wrong password" });
+      }
+  
+      const id = user[0].id;
+      const name = user[0].name;
+      const email = user[0].email;
+  
+      const userData = { id, name, email, accessToken: "" };
+  
+      const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d"
+      });
+      const refreshToken = jwt.sign(userData, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d"
+      });
+  
+      await Users.update(
+        { refresh_token: refreshToken },
+        {
+          where: {
+            id: id
+          }
+        }
+      );
+  
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000
+      });
+  
+      userData.accessToken = accessToken;
+      res.json(userData);
+    } catch (error) {
+      res.status(500).json({ msg: "Internal server error" });
+    }
+  };
+  
     
     export const Logout = (async(req, res) => {
         const refreshToken = req.cookies.refreshToken;
