12c2eac64947325caa8f414506b0d60bbc5b08ca
yuukominaru <patriciaaela@gmail.com>
2023-06-12 21:54:40 +0700

    changed latlon toast to log (yg sblmny mapsviewmodel yg kecommit) mapsviewmodel bnyk yg dicomment (must be fixed)

--- app/src/main/java/com/capstone/sopanfinder/view/maps/MapsActivity.kt
+++ app/src/main/java/com/capstone/sopanfinder/view/maps/MapsActivity.kt
@@ -1,40 +1,24 @@
 package com.capstone.sopanfinder.view.maps
 
-import android.content.ContentValues
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
 import com.capstone.sopanfinder.R
-import com.capstone.sopanfinder.api.ApiConfig
-import com.capstone.sopanfinder.api.Login
-import com.capstone.sopanfinder.api.WeatherResponse
 import com.capstone.sopanfinder.databinding.ActivityMapsBinding
-import com.capstone.sopanfinder.preference.UserPreference
-import com.capstone.sopanfinder.view.ViewModelFactory
 import com.capstone.sopanfinder.view.favorite.FavoriteActivity
 import com.capstone.sopanfinder.view.graph.GraphActivity
 import com.capstone.sopanfinder.view.home.HomeActivity
-import com.capstone.sopanfinder.view.login.LoginViewModel
 import com.capstone.sopanfinder.view.profile.ProfileActivity
-import com.capstone.sopanfinder.view.result.ResultActivity
-import com.capstone.sopanfinder.view.signup.SignupViewModel
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import kotlin.math.log
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -50,14 +34,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         supportActionBar?.setTitle("SoPan Finder")
 
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-
-
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -70,16 +51,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(location).title("You Are Here!"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
 
-
-        Toast.makeText(applicationContext, "Latitude : $lat Longitude: $lon", Toast.LENGTH_SHORT).show()
-
+//        Toast.makeText(applicationContext, "Latitude : $lat Longitude: $lon", Toast.LENGTH_SHORT).show()
+        Log.i("MapsActivity", "Latitude : $lat Longitude: $lon")
 
         val latitude = lat.toFloat()
         val longitude = lon.toFloat()
 
         //Get Weather Data From API, send to logcat and display graph data
         binding.confirmButton.setOnClickListener{
-
             mapsViewModel.getWeatherData(latitude, longitude)
             startActivity(Intent(this@MapsActivity, GraphActivity::class.java))
             finish()
@@ -97,8 +76,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.favorite_menu -> startActivity(Intent(this@MapsActivity, FavoriteActivity::class.java))
-            R.id.profile_menu -> startActivity(Intent(this@MapsActivity, ProfileActivity::class.java))
+            R.id.favorite_menu -> {
+                startActivity(Intent(this@MapsActivity, FavoriteActivity::class.java))
+                finish()
+            }
+            R.id.profile_menu -> {
+                startActivity(Intent(this@MapsActivity, ProfileActivity::class.java))
+                finish()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
